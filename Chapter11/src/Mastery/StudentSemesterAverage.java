becc8c6afc92e51a5c53a3057fbd947a8f09c205
38018005 <38018005@L815WD220021.edu.cbe.ab.ca>
2026-03-30 14:05:06 -0600

    2:05

--- Chapter11/src/Mastery/StudentSemesterAverage.java
+++ Chapter11/src/Mastery/StudentSemesterAverage.java
@@ -7,26 +7,37 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class StudentSemesterAverage {
 
 	private JFrame frame;
-	private JTextField stuName;
+	private JTextField studentName;
 	private JTextField gradeLevel;
 	private JTextField semNumber;
 	private JTextField grade1;
 	private JTextField grade2;
 	private JTextField grade3;
 	private JTextField grade4;
-	private JTextField average;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -51,7 +62,7 @@ public class StudentSemesterAverage {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 376);
+		frame.setBounds(100, 100, 632, 376);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
@@ -95,51 +106,139 @@ public class StudentSemesterAverage {
 		
 		JLabel avgLabel = new JLabel("Average:");
 		avgLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
-		avgLabel.setBounds(10, 186, 152, 17);
+		avgLabel.setBounds(10, 186, 62, 17);
 		panel.add(avgLabel);
 		
-		stuName = new JTextField();
-		stuName.setBounds(154, 10, 270, 20);
-		panel.add(stuName);
-		stuName.setColumns(10);
+		studentName = new JTextField();
+		studentName.setBounds(154, 10, 452, 20);
+		panel.add(studentName);
+		studentName.setColumns(10);
 		
 		gradeLevel = new JTextField();
 		gradeLevel.setColumns(10);
-		gradeLevel.setBounds(154, 35, 270, 20);
+		gradeLevel.setBounds(154, 35, 452, 20);
 		panel.add(gradeLevel);
 		
 		semNumber = new JTextField();
 		semNumber.setColumns(10);
-		semNumber.setBounds(154, 60, 270, 20);
+		semNumber.setBounds(154, 60, 452, 20);
 		panel.add(semNumber);
 		
 		grade1 = new JTextField();
 		grade1.setColumns(10);
-		grade1.setBounds(154, 85, 270, 20);
+		grade1.setBounds(154, 85, 452, 20);
 		panel.add(grade1);
 		
 		grade2 = new JTextField();
 		grade2.setColumns(10);
-		grade2.setBounds(154, 110, 270, 20);
+		grade2.setBounds(154, 110, 452, 20);
 		panel.add(grade2);
 		
 		grade3 = new JTextField();
 		grade3.setColumns(10);
-		grade3.setBounds(154, 136, 270, 20);
+		grade3.setBounds(154, 136, 452, 20);
 		panel.add(grade3);
 		
 		grade4 = new JTextField();
 		grade4.setColumns(10);
-		grade4.setBounds(154, 161, 270, 20);
+		grade4.setBounds(154, 161, 452, 20);
 		panel.add(grade4);
 		
-		average = new JTextField();
-		average.setColumns(10);
-		average.setBounds(154, 186, 270, 20);
+		JTextArea summary = new JTextArea();
+		summary.setBackground(new Color(240, 240, 240));
+		summary.setBounds(10, 214, 596, 64);
+		panel.add(summary);
+		
+		JTextArea average = new JTextArea();
+		average.setBackground(new Color(240, 240, 240));
+		average.setBounds(77, 186, 347, 17);
 		panel.add(average);
 		
-		JTextArea summary = new JTextArea();
-		summary.setBounds(10, 214, 414, 64);
-		panel.add(summary);
+		File stuAvgFile = new File("../Chapter11/SemAverage");
+		
+		JButton save = new JButton("Save to File");
+		save.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				String stuName = studentName.getText();
+				String grdLevel = gradeLevel.getText();
+				String semNum = semNumber.getText();
+				
+				int g1 = Integer.parseInt(grade1.getText());
+				int g2 = Integer.parseInt(grade2.getText());
+				int g3 = Integer.parseInt(grade3.getText());
+				int g4 = Integer.parseInt(grade4.getText());
+				
+				int avg = (g1 + g2 + g3 + g4) / 4;
+				
+				average.setText(Integer.toString(avg));
+				
+				try
+				{
+					FileOutputStream out = new FileOutputStream(stuAvgFile);
+					ObjectOutputStream writeStu = new ObjectOutputStream(out);
+					
+					writeStu.writeObject("Name: " + stuName + ", Grade Level: " + grdLevel + ", Semester: " + semNum + ", Grades: " + g1 + ", " + g2 + ", " + g3 + ", " + g4 + ". Average: " + avg + "%");
+					
+					writeStu.close();
+				}
+				
+				catch (FileNotFoundException e1) 
+				{
+					// Runs if the file cannot be found or created
+					System.out.println("File Does not Exist.");
+					System.err.println("IOException: " + e1.getMessage());
+				}
+				
+				catch (IOException e1) 
+				{
+					// Runs if there is a problem writing/reading the file
+					System.out.println("Cannot Write in File");
+					System.err.println("IOException: " + e1.getMessage());
+				}
+			}
+		});
+		save.setFont(new Font("Tahoma", Font.BOLD, 12));
+		save.setBounds(148, 289, 127, 37);
+		panel.add(save);
+		
+		JButton preview = new JButton("View File Contents");
+		preview.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				String text;
+				FileReader in;
+				BufferedReader readFile;
+				
+				try
+				{
+					in = new FileReader(stuAvgFile);
+					readFile = new BufferedReader(in);
+					
+					while((text = readFile.readLine()) != null)
+					{
+						summary.setText(text);
+					}
+				}
+				
+				catch(FileNotFoundException e1)
+				{
+					// Runs if the file cannot be found
+					System.out.println("File does not exist.");
+					System.err.println("fileNotFoundException: " + e1.getMessage());
+				}
+				
+				catch (IOException e1) 
+				{
+					// Runs if there is a problem reading the file
+					System.out.println("Problem reading file.");
+					System.err.println("IOException: " + e1.getMessage());
+				}
+				
+			}
+		});
+		preview.setFont(new Font("Tahoma", Font.BOLD, 12));
+		preview.setBounds(284, 289, 176, 37);
+		panel.add(preview);
 	}
 }
